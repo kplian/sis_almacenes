7c01bd719e979feed3b989ac8b3ae89abafdb4c8
Chacho3x <ariel.ayaviri.omonte@gmail.com>
2013-02-03 04:33:12 -0400

    -#5: CRUD de materiales(items) Se formateo el codigo Se corrigio un bug cuando se editaba un item, el dialog save no mostraba la clasificacion del item y la guardaba como null. Se cambio la forma de eliminar un item en la funcion ime. Ahora la eliminacion de items es logica y no fisica.

--- base/funciones/ft_item_sel.sql
+++ base/funciones/ft_item_sel.sql
@@ -46,21 +46,22 @@ BEGIN
                     
         begin
             --Sentencia de la consulta
-            v_consulta:='select
-                        item.id_item,
-                        item.id_clasificacion,
-                        cla.nombre as desc_clasificacion,
-                        cla.codigo_largo,
-                        item.nombre,
-                        item.codigo,
-                        item.descripcion,
-                        item.palabras_clave,
-                        item.codigo_fabrica,
-                        item.observaciones,
-                        item.numero_serie                       
-                        from alm.titem item
-                        left join alm.tclasificacion cla on cla.id_clasificacion=item.id_clasificacion
-                        where ';
+            v_consulta:='
+            	select
+                	item.id_item,
+                    item.id_clasificacion,
+                    cla.nombre as desc_clasificacion,
+                    cla.codigo_largo,
+                    item.nombre,
+                    item.codigo,
+                    item.descripcion,
+                    item.palabras_clave,
+                    item.codigo_fabrica,
+                    item.observaciones,
+                    item.numero_serie                       
+                from alm.titem item
+                left join alm.tclasificacion cla on cla.id_clasificacion=item.id_clasificacion
+                where item.estado_reg = ''activo'' and ';
            
             --Definicion de la respuesta
             v_consulta:=v_consulta||v_parametros.filtro;
@@ -68,7 +69,6 @@ BEGIN
 			
             --Devuelve la respuesta
             return v_consulta;
-            
                        
         end;
     /*********************************   
@@ -80,17 +80,18 @@ BEGIN
             
     elseif(p_transaccion='SAL_ARB_SEL')then
     	begin
-        	v_consulta:='select
-						it.id_item,
-						it.nombre,
-						it.codigo,
-						it.id_clasificacion as id_clasificacion_fk,
-                        (it.id_clasificacion::varchar||''_''||it.id_item::varchar)::varchar as id_clasificacion,
-                        (''item'')::varchar as tipo_nodo						
-						from alm.titem it
-						inner join segu.tusuario usu1 on usu1.id_usuario = it.id_usuario_reg
-						left join segu.tusuario usu2 on usu2.id_usuario = it.id_usuario_mod
-                        where it.id_clasificacion= '|| v_parametros.id_clasificacion||' and ';
+        	v_consulta:='
+            	select
+					it.id_item,
+                    it.nombre,
+                    it.codigo,
+                    it.id_clasificacion as id_clasificacion_fk,
+                    (it.id_clasificacion::varchar||''_''||it.id_item::varchar)::varchar as id_clasificacion,
+                    (''item'')::varchar as tipo_nodo						
+                from alm.titem it
+                inner join segu.tusuario usu1 on usu1.id_usuario = it.id_usuario_reg
+                left join segu.tusuario usu2 on usu2.id_usuario = it.id_usuario_mod
+                where it.estado_reg = ''activo'' and it.id_clasificacion= '|| v_parametros.id_clasificacion||' and ';
             --Definicion de la respuesta
             v_consulta:=v_consulta||v_parametros.filtro;
             --Devuelve la respuesta
@@ -108,20 +109,21 @@ BEGIN
                     
         begin
             --Sentencia de la consulta
-            v_consulta:='select
-                        item.id_item,
-                        item.id_clasificacion,
-                        cla.nombre as desc_clasificacion,
-                        cla.codigo_largo,
-                        item.nombre,
-                        item.descripcion,
-                        item.palabras_clave,
-                        item.codigo_fabrica,
-                        item.observaciones,
-                        item.numero_serie                       
-                        from alm.titem item, alm.tclasificacion cla
-                        where item.id_clasificacion = cla.id_clasificacion 
-                        and cla.id_clasificacion_fk is not NULL and ';
+            v_consulta:='
+            	select
+                	item.id_item,
+                    item.id_clasificacion,
+                    cla.nombre as desc_clasificacion,
+                    cla.codigo_largo,
+                    item.nombre,
+                    item.descripcion,
+                    item.palabras_clave,
+                    item.codigo_fabrica,
+                    item.observaciones,
+                    item.numero_serie                       
+                from alm.titem item, alm.tclasificacion cla
+                where item.estado_reg = ''activo'' and item.id_clasificacion = cla.id_clasificacion 
+                and cla.id_clasificacion_fk is not NULL and ';
            
             --Definicion de la respuesta
             v_consulta:=v_consulta||v_parametros.filtro;
@@ -144,10 +146,11 @@ BEGIN
 
         begin
             --Sentencia de la consulta de conteo de registros
-            v_consulta:='select count(item.id_item)
-                        from alm.titem item, alm.tclasificacion cla
-                        where item.id_clasificacion = cla.id_clasificacion 
-                        and cla.id_clasificacion_fk is not NULL and ';
+            v_consulta:='
+            	select count(item.id_item)
+                from alm.titem item, alm.tclasificacion cla
+                where item.estado_reg = ''activo'' and item.id_clasificacion = cla.id_clasificacion 
+                and cla.id_clasificacion_fk is not NULL and ';
            
             --Definicion de la respuesta           
             v_consulta:=v_consulta||v_parametros.filtro;
@@ -168,9 +171,10 @@ BEGIN
 
         begin
             --Sentencia de la consulta de conteo de registros
-            v_consulta:='select count(item.id_item)
-                        from alm.titem item, alm.tclasificacion cla
-                        where item.id_clasificacion = cla.id_clasificacion and ';
+            v_consulta:='
+            	select count(item.id_item)
+                from alm.titem item, alm.tclasificacion cla
+                where item.estado_reg = ''activo'' and item.id_clasificacion = cla.id_clasificacion and ';
            
             --Definicion de la respuesta           
             v_consulta:=v_consulta||v_parametros.filtro;
