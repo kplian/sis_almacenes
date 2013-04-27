d85342f69c16403ef09d35ed86fb90388af50bae
Chacho3x <ariel.ayaviri.omonte@gmail.com>
2013-04-26 21:11:20 -0400

    -#56: Reporte de Existencias.
    Modificacion de las funciones de obtencion de saldos fisicos y valorados para delimitar el saldo del item a una fecha especifica.

--- base/funciones/f_get_valorado_item.sql
+++ base/funciones/f_get_valorado_item.sql
@@ -1,14 +1,6 @@
-CREATE OR REPLACE FUNCTION alm.f_get_valorado_item (
-  p_id_item integer,
-  p_id_almacen integer,
-  p_valoracion varchar,
-  p_cantidad_sol numeric,
-  out r_costo_valorado numeric,
-  out r_cantidad_valorada numeric,
-  out r_id_movimiento_det_val_desc integer
-)
-RETURNS record AS
-$body$
+CREATE OR REPLACE FUNCTION alm.f_get_valorado_item(IN p_id_item integer, IN p_id_almacen integer, IN p_valoracion character varying, IN p_cantidad_sol numeric, IN p_fecha_mov date, OUT r_costo_valorado numeric, OUT r_cantidad_valorada numeric, OUT r_id_movimiento_det_val_desc integer)
+  RETURNS record AS
+$BODY$
 /**************************************************************************
 
  SISTEMA:   SISTEMA DE GESTION DE ALMACENES
@@ -39,8 +31,8 @@ BEGIN
     
    
     IF (p_valoracion = 'PP') THEN
-        v_saldo_fisico = alm.f_get_saldo_fisico_item(p_id_item, p_id_almacen);
-        v_saldo_valorado = alm.f_get_saldo_valorado_item(p_id_item, p_id_almacen);
+        v_saldo_fisico = alm.f_get_saldo_fisico_item(p_id_item, p_id_almacen, p_fecha_mov);
+        v_saldo_valorado = alm.f_get_saldo_valorado_item(p_id_item, p_id_almacen, p_fecha_mov);
         
         IF (v_saldo_fisico = 0) THEN
           r_costo_valorado = 0;
@@ -113,110 +105,7 @@ BEGIN
     END IF;
 
   return;
-   
---    DROP TABLE IF EXISTS  tt_item_costo_unitario;
-    
-        
-    /***********************************
-    # Consulta para obtener el costo unitario para el item: p_id_item en funcion al parametro
-    # del criterio de valoracion enviado: FIFO, LIFO o Promedio 
-    # Cuando el saldo es positivo devuelve la cantidad que es un valor numeric,
-    # si el item no existe devulve 0.
-    ***********************************/
-   
-/*
-    if(p_criterio_valoracion = 1) then --FIFO: 1
-      begin
-        
-        -- Obtener el costo_unitario del primer ingreso      
-        v_consulta = '
-        WITH item_ingresos_valorado
-          AS  (
-                SELECT movdet.id_movimiento_det, movdet.id_item, mov.id_movimiento, movdet.costo_unitario
-                FROM alm.tmovimiento_det movdet
-                INNER JOIN alm.tmovimiento mov ON mov.id_movimiento = movdet.id_movimiento
-                INNER JOIN alm.tmovimiento_tipo movti ON mov.id_movimiento_tipo = movti.id_movimiento_tipo
-                WHERE movti.codigo ilike ''INGRESO'' and movdet.id_item ='|| p_id_item ||'      
-                ORDER BY movdet.id_movimiento_det asc limit 1 
-              )
-          SELECT * INTO TEMP tt_item_costo_unitario FROM  item_ingresos_valorado';
-      execute(v_consulta);
-        
-      end;
-    elsif(p_criterio_valoracion = 2) then --LIFO: 2
-      begin
-        
-        -- Obtener el costo_unitario del ultimo ingreso 
-        v_consulta = '
-        WITH item_ingresos_valorado
-          AS  (
-                SELECT movdet.id_movimiento_det, movdet.id_item, mov.id_movimiento, movdet.costo_unitario
-                FROM alm.tmovimiento_det movdet
-                INNER JOIN alm.tmovimiento mov ON mov.id_movimiento = movdet.id_movimiento
-                INNER JOIN alm.tmovimiento_tipo movti ON mov.id_movimiento_tipo = movti.id_movimiento_tipo
-                WHERE movti.codigo ilike ''INGRESO'' and movdet.id_item ='|| p_id_item ||'
-                ORDER BY movdet.id_movimiento_det asc limit 1 
-              )
-          SELECT * INTO TEMP tt_item_costo_unitario  FROM  item_ingresos_valorado';
-      execute(v_consulta);
-        
-        end;
-    
-    elsif(p_criterio_valoracion = 3) then --Promedio: 3
-      begin
-        
-        -- Obtener el costo_unitario promedio de todos los ingresos
-        v_consulta = '
-        WITH item_ingresos_valorado
-          AS  (
-                SELECT movdet.id_item, array_agg(mov.id_movimiento) AS id_movimiento,
-                sum(movdet.cantidad) AS cantidad_total, sum(movdet.costo_unitario * movdet.cantidad) AS costo_unitario_total
-                FROM alm.tmovimiento_det movdet
-                INNER JOIN alm.tmovimiento mov ON mov.id_movimiento = movdet.id_movimiento
-                INNER JOIN alm.tmovimiento_tipo movti ON mov.id_movimiento_tipo = movti.id_movimiento_tipo
-                WHERE movti.codigo ilike ''INGRESO'' and movdet.id_item ='|| p_id_item ||'
-                GROUP BY id_item
-              ),
-          costo_promedio
-          AS (    
-              SELECT *, (costo_unitario_total /cantidad_total) AS costo_unitario FROM  item_ingresos_valorado
-              )
-        SELECT * INTO TEMP tt_item_costo_unitario  FROM costo_promedio';
-      execute(v_consulta);
-        
-        end;
-        
-    ELSE
-      raise exception 'Opcion de valoracion de item invalida.';
 
-    END IF;
-        
-    SELECT costo_unitario INTO v_item_costo_unitario FROM tt_item_costo_unitario;    
-    
-    IF v_item_costo_unitario <= 0 OR v_item_costo_unitario IS NULL THEN
-        v_item_costo_unitario = 0;
-    END IF;    
-    
-    RETURN v_item_costo_unitario;
-  
-    -- En caso de que se requiera retornar un record (RETURNS SETOF  record AS $$) 
-    -- con la información de las cantidaddes de los ingresos y salidas ademas del saldo. 
-    
-    --RETURN QUERY SELECT * FROM tt_item_saldo;
-    --SETOF  record
-    */
-    
-    --El SELECT para obtener los valores seria:
-    /*
-      SELECT * gem.f_get_valorado_item ( 5 ) 
-         f ( id_item  integer,              
-             id_movimiento_ingresos integer[],
-             id_movimiento_salidas integer[],
-             suma_ingresos numeric,
-             suma_salidas numeric,
-             saldo numeric
-          );
-    */
 EXCEPTION         
 
   WHEN OTHERS THEN
@@ -232,9 +121,8 @@ EXCEPTION
       raise exception '%',v_resp;
 
 END;
-$body$
-LANGUAGE 'plpgsql'
-VOLATILE
-CALLED ON NULL INPUT
-SECURITY INVOKER
-COST 100;
+$BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100;
+ALTER FUNCTION alm.f_get_valorado_item(integer, integer, character varying, numeric, date)
+  OWNER TO postgres;
